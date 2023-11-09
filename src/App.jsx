af0eb8910600046065f599f091ed4d04e99e93d3
Bruno Daniel <117387729+BrunoDaniell@users.noreply.github.com>
2023-11-09 01:25:29 +0000

    Primeiro dia

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img src ="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+      </div>
+      <br/>
+    </>
+  )
+}
+
+export default App
