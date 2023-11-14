16026f40c683401d1bbbd8c62d7d95dfa004ba18
Bruno Daniel <117387729+BrunoDaniell@users.noreply.github.com>
2023-11-14 00:02:18 +0000

    criação do ncomponente card

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './Components/Card/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,9 +11,15 @@ function App() {
     <>
       <div className="card">
         <h2>Rick Sanchez</h2> 
+          <div className="tags">
+            <div className="tag">teste1</div>
+            <div className="tag">teste2</div>            
+            <div className="tag">teste3</div>    
+            </div> 
         <img src ="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
       </div>           
       <br/>
+      <Card />
     </>
   )
 }
