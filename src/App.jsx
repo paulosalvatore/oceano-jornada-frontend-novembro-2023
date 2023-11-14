ef6e362f7e32181a59e7a63d9c38f923f595310f
Bruno Daniel <117387729+BrunoDaniell@users.noreply.github.com>
2023-11-14 00:46:27 +0000

    extração do conteudo para compopnentes

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="card">
-        <h2>Rick Sanchez</h2> 
-          <div className="tags">
-            <div className="tag">teste1</div>
-            <div className="tag">teste2</div>            
-            <div className="tag">teste3</div>    
-            </div> 
-        <img src ="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
-      </div>           
+    <>         
       <br/>
+      <Card />
       <Card />
     </>
   )
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,3 +1,11 @@
 export default function Card(){
-    return <div>Card está funcionando</div>
+    return <div className="card">
+    <h2>Rick Sanchez</h2> 
+      <div className="tags">
+        <div className="tag">teste1</div>
+        <div className="tag">teste2</div>            
+        <div className="tag">teste3</div>    
+        </div> 
+    <img src ="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+  </div>  
 }
